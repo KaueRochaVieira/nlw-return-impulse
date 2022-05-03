76eb75a8a706f7de43e4acd87b7aec52ac2af82d
Kauê Rocha <kauetoll3@gmail.com>
2022-05-03 20:34:14 -0300

    added assets folder

---
+++ src/componentes/WidgetForm.tsx
@@ -0,0 +1,38 @@
+import { CloseButton } from './CloseButton'
+
+const feedbackTypes = {
+  BUG: {
+    title: 'Problema'
+  },
+  IDEA: {
+    title: 'Ideia'
+  },
+  OTHER: {
+    title: 'Outro'
+  }
+}
+
+export function WidgetForm() {
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-column items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      <header>
+        <span className="text-xl leading-6">Deixe seu feedback</span>
+
+        <CloseButton />
+      </header>
+
+      <div className="flex py-8 gap-2 w-full"></div>
+
+      <p>Hello Word</p>
+      <footer className="text-xs text-neutral-400">
+        Feito com 🖤 pela{' '}
+        <a
+          className="underline underline-offset-2"
+          href="https://rocketseat.com.br"
+        >
+          Rocketseat
+        </a>
+      </footer>
+    </div>
+  )
+}
